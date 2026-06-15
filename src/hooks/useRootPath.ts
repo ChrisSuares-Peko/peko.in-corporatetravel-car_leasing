8065f07cbd45e4e8197b5e9d91183f271b091870
ChrisSuares-Peko <Chris@peko.one>
2026-06-15 13:28:07 +0530

    commit

--- src/hooks/useRootPath.ts
+++ src/hooks/useRootPath.ts
@@ -1,19 +1,3 @@
-// useRootPath hook
-import { UserRole } from '@customtypes/general';
-import { useAppSelector } from '@src/hooks/store';
 import { paths } from '@src/routes/paths';
 
-export const useRootPath = () => {
-    const { role } = useAppSelector(state => state.reducer.auth);
-
-    if (import.meta.env.VITE_PROTOTYPE_MODE === 'true') return paths.dashboard.home;
-
-    switch (role) {
-        case UserRole.CORPORATE:
-            return paths.dashboard.home;
-        case UserRole.SYSTEM:
-            return paths.systemUser.dashboard;
-        default:
-            return '/auth/login';
-    }
-};
+export const useRootPath = () => paths.dashboard.home;
